84bfded96a01c2c71dc7ebed451ed39c132295ae
seong3056 <aktnf207@naver.com>
2024-02-13 17:43:12 +0900

    index.html, address.py   - index.html     페이지 기본적인 구성 및 데이터 출력, 지역별로 나오게 시도   - address.py     함수화하여 지역구에 접근할수 있도록 설정

--- api/address.py
+++ api/address.py
@@ -1,14 +1,32 @@
 import requests
 
-key = ""
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+key = "6801e19bdc6b58d340eb8e0e336a3dad"
 url = "https://dapi.kakao.com/v2/local/search/keyword.json"
 headers = {
     "Authorization": "KakaoAK "+key,
     "content-type": "application/json;charset=UTF-8"
 }
 params = {
-    "query" : "예식",    
-    
+    "query" : "서울 마포구 웨딩",    
 }
-data = requests.get(url, params=params, headers=headers).json()["documents"]
-print(data)
+
+data_list = requests.get(url, params=params, headers=headers).json()["documents"]
+print(data_list)
+
+url = data_list["place_url"]
+print(url)
+data = requests.get(url).json()
+
+def search(address1, address2, keyword):
+    params = {
+    "query" : f"{address1} {address2} {keyword}",    
+    }
+
+    data_list = requests.get(url, params=params, headers=headers).json()["documents"]
+    return data_list
+
