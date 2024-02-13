7b9f4790bbcee2a9e978271bfdf15efe3503f394
jjhwk <jjhwk@naver.com>
2024-02-13 17:54:51 +0900

    address.py - 카카오 키워드 검색의 key값 주석

--- api/address.py
+++ api/address.py
@@ -12,3 +12,13 @@ params = {
 }
 data = requests.get(url, params=params, headers=headers).json()["documents"]
 print(data)
+
+# address_name : 주소
+# category_name : 분류("가정,생활 > 결혼 > 예식장")
+# id : 아이디
+# phone : 전화번호
+# place_name : 상호 명
+# place_url : url 주소
+# road_address_name': 도로명 주소
+# x : 경도
+# y : 위도
