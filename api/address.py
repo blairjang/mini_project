396c93935a4615b8647c338737bc012de6ac25e9
seong3056 <aktnf207@naver.com>
2024-02-13 15:00:54 +0900

    address.py
    키워드를 통해 관련 장소 가져오는 .py 생성

---
+++ api/address.py
@@ -0,0 +1,14 @@
+import requests
+
+key = ""
+url = "https://dapi.kakao.com/v2/local/search/keyword.json"
+headers = {
+    "Authorization": "KakaoAK "+key,
+    "content-type": "application/json;charset=UTF-8"
+}
+params = {
+    "query" : "예식",    
+    
+}
+data = requests.get(url, params=params, headers=headers).json()["documents"]
+print(data)
