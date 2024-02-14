f8c9ea84f1c6ba83dd3ce0829b87f25c9ccdbd66
jjhwk <jjhwk@naver.com>
2024-02-14 10:09:09 +0900

    delete app private

--- wedding/settings.py
+++ wedding/settings.py
@@ -37,11 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-<<<<<<< HEAD
     "review",
-=======
-    "info",
->>>>>>> 7bf514de3ea6c372a1c2ae975458f3abfc7be077
+    "info",    
 ]
 
 MIDDLEWARE = [
