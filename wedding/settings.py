2ded92bbc32d2fbd110bc95e2bdb623afc781cfe
seong3056 <aktnf207@naver.com>
2024-02-14 09:54:13 +0900

    reset commit

--- wedding/settings.py
+++ wedding/settings.py
@@ -37,11 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-<<<<<<< HEAD
-    "review",
-=======
     "info",
->>>>>>> 7bf514de3ea6c372a1c2ae975458f3abfc7be077
 ]
 
 MIDDLEWARE = [
