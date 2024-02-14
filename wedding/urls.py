30ab589e841e6fbbad0948f9852ef8a0efe13f06
jjhwk <jjhwk@naver.com>
2024-02-14 10:01:35 +0900

    commit

--- wedding/urls.py
+++ wedding/urls.py
@@ -15,17 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-<<<<<<< HEAD
-from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path(),
-=======
 from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("wedding/", include("info.urls")),
->>>>>>> 7bf514de3ea6c372a1c2ae975458f3abfc7be077
+    path("reviews/", include("review.urls")),
 ]
